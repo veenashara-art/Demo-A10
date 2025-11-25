ab07ae356caaba922d74dced29b565a431a87030
veena s <veena s@DESKTOP-GNIRPH2>
2025-11-25 16:58:37 +0530

    Second Commit

--- src/test/java/GitPractice/DemoPractice.java
+++ src/test/java/GitPractice/DemoPractice.java
@@ -6,6 +6,7 @@ public class DemoPractice {
 @Test	
 public void m1() {
 	System.out.println("HI");
+	System.out.println("Good morning");
 	
 }
 
---
+++ src/test/java/GitPractice/DemoPractice2.java
@@ -0,0 +1,15 @@
+package GitPractice;
+
+import org.testng.annotations.Test;
+
+public class DemoPractice2 {
+	@Test	
+	public void m1() {
+		System.out.println("HI");
+		System.out.println("Good morning,second commit");
+		
+		
+	}
+
+
+}
